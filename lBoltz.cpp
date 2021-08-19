588f4e49317944b52e0aa58ecf18f0f543672a4f
sean-harding <60259338+sean-harding@users.noreply.github.com>
2021-08-19 18:47:11 +0100

    Fleshed out comments

--- lBoltz.cpp
+++ lBoltz.cpp
@@ -9,17 +9,6 @@ typedef std::vector<double> vec;
 typedef std::vector<int> ivec;
 typedef std::vector<latt2D> lattice;
 
-//Map a 2-parameter function across multiple argument pairs given in arg1 and arg2
-template<class FUNC, class T>
-auto map(FUNC func, const std::vector<T> &arg1, const std::vector<T> &arg2)
-{
-    int nvalues = std::min( arg1.size(), arg2.size());
-    auto result = std::vector<T>(nvalues);
-    for (int i=0; i<nvalues; ++i)
-    {result[i] = func(arg1[i], arg2[i]);}
-    return result;
-}
-
 //Constructor function for lattice site objects. See header file for member definitions
 latt2D::latt2D(int index,double dens, vec iVel,double om, vec &wx, vec &wy,char type,int lx,int ly){
     //Initialize the current state of the system
@@ -27,18 +16,12 @@ latt2D::latt2D(int index,double dens, vec iVel,double om, vec &wx, vec &wy,char 
     this->vDist_new = vec {0,0,0,0,0,0,0,0,0};
     this->index = index;           
     this->type = type;
-    this->om = om;
     //Initialize macroscopic variables
     this->dens = dens;
     this->fVel = iVel;
-
     //This works out nearest-neighbors and next-nearest neighbors. index = y*ly + x
     int x = index%lx;
     int y = index/lx;
-        
-    //ivec new_x = {x,(x+1)%lx,x,(x-1)%lx,x,(x+1)%lx,(x-1)%lx,(x-1)%lx,(x+1)%lx};
-    //ivec new_y = {y,y,(y-1)%ly,y,(y+1)%ly,(y-1)%ly,(y-1)%ly,(y+1)%ly,(y+1)%ly};
-    
     ivec new_x = {x,(x-1)%lx,x,(x+1)%lx,x,(x-1)%lx,(x+1)%lx,(x+1)%lx,(x-1)%lx};
     ivec new_y = {y,y,(y+1)%ly,y,(y-1)%ly,(y-1)%ly,(y-1)%ly,(y+1)%ly,(y+1)%ly};
     for(int i=0; i<new_x.size();i++){
@@ -53,24 +36,16 @@ latt2D::latt2D(int index,double dens, vec iVel,double om, vec &wx, vec &wy,char 
     this->collide(wx,wy,[](double arg1, double arg2){return arg2;});
     this->vDist_new = this->vDist;  //Check to make sure these are not pointing to the same thing
 }   
-/*  The velocity vector in position [i] in the array points from zero to adjacent sites. The order of these sites
-    is:
-        6 2 5
-        3 0 1
-        7 4 8
-    for example, the magnitude of the velocity vector (-1,0) is stored in array index [3].
-vec reflect = {0,3,4,1,2,7,8,5,6};
-*/
 
-//This function takes a lattice site l and streams particles from it to neighbors
+//This function takes a lattice site l and streams particles to it from neighbors
 void latt2D::stream(lattice &l, vec &refl){
     for(int i=0;i<(this->vDist).size();i++){
-        int whichSite = this->streamChannels[i];  //Which site to stream from. Work this out when objects
+        int whichSite = this->streamChannels[i];              //Which site to stream from
         if(l[whichSite].type != 'w'){                         //Check if there is a solid wall at that pixel
         this->vDist_new[i] = l[whichSite].vDist[i];
         }
-        else{                                                 //Reflect particles off of the wall
-        this->vDist_new[i] = this->vDist[refl[i]];            //Reflect particles off of the wall
+        else{                                                 //Reflect particles off of any walls
+        this->vDist_new[i] = this->vDist[refl[i]];            
         }
     }
 }
@@ -81,52 +56,41 @@ This function relaxes the state of the fluid at a given lattice site depending o
 template<class FUNC> void latt2D::collide(vec &wx, vec &wy,FUNC f){
     /*
     Relax particles at a given lattice site
-    STEP 1.) Calculate the new macroscopic quanities from vDist_new
+    STEP 1.) Calculate the new macroscopic quanities from vDist_new (this is done externally to the function to reuse the same code for initialization)
     STEP 2.) Calculate equilibrium distributions from macroscopic quantities, (F*_eq)
     STEP 3.) Update distribution as F_new = (1-t/T)*F_old +(t/T)*F*_eq
-    
-    If t/T is kept fixed, then we can pre-compute t/T to avoid having to perform a bunch of similar divisions
-    at each iteration. This is done in the current implementation but should be changed if we want to be able to
-    change the timestep 
     */
 
-    //STEP 1
-    //this->update('d',wx,wy);
-    //this->update('v',wx,wy);
     //STEP 2
-    //mag_u is |velocity|^2
+    //Calculate |velocity|^2
     double mag_u = 0.0;
     for(int i =0;i<(this->fVel).size();i++){
         mag_u += pow(this->fVel[i],2.0);        
     }
-    //Define lambda expression for the f1->f4
+    //These are expressions for the equilibrium distribution functions for f_1->f_4
     auto expr1 = [this, &mag_u](double a,double b){
         return this->dens*(2+6*a+9*b+3*mag_u)/18;};
     
-    //Define lambda expression for the f5->f8
+    //These are expressions for the equilibrium distribution functions for f_5->f_8
     auto expr2 = [this, &mag_u](double a,double b){
         return this->dens*(1+3*(a+b)+9*a*b+3*mag_u)/36;};
     
-    //These are the function arguments
+    //These are the function arguments to calculate each discretant of the distribution function
     std::vector<double> args1 = {this->fVel[0],this->fVel[1],-1.0*this->fVel[0],-1.0*this->fVel[1]};
     std::vector<double> args2 = {pow(this->fVel[0],2.0),pow(this->fVel[1],2.0),pow(this->fVel[0],2.0),pow(this->fVel[1],2.0)};
     std::vector<double> args3 = {this->fVel[1],-1.0*this->fVel[0],-1.0*this->fVel[1],this->fVel[0]};
     
-    //STEP 3
-    //The following could be written more cleanly as each line of the if/else has the same structure
-    //Problem in the arguments??
+    //STEP 3. The function f is used to perform the update
     for(int i=0;i<this->vDist.size();i++){
         if(i==0){
             this->vDist[i] = f(this->vDist_new[i],(2*3*mag_u)*this->dens);}
-            //this->vDist[i] = (1-this->om)*this->vDist_new[i] + this->om*(2-3*mag_u)*this->dens;}
         else if(i<5){
             this->vDist[i] = f(this->vDist_new[i],expr1(args1[i-1],args2[i-1]));}
-            //this->vDist[i] = (1-this->om)*this->vDist_new[i] + this->om*expr1(args1[i-1],args2[i-1]);}
         else{
             this->vDist[i] = f(this->vDist_new[i],expr2(args1[i-5],args3[i-5]));}            
-            //this->vDist[i] = (1-this->om)*this->vDist_new[i] + this->om*expr2(args1[i-5],args3[i-5]);}     //f2 through f8 inclusive 
     }
 }
+//Is used to update the density (with flag 'd') or velocity (with flag 'v')
 void latt2D::update(char which, vec &wx, vec &wy){
     if(which=='d'){
         this->dens = 0;
@@ -150,30 +114,42 @@ int main(int argc, char *argv[]){
     with the NS solution being an exponential decay of the velocity and density profiles. tg_x, tg_y and tg_d
     are the values of the x-component of velocity, y-component of velocity and density of the TG solution at t=0
     */
+    //------------------|TEST CASE PARAMETERS|------------------
     double u0 = 0.1;
     double kx = M_PI/10;
     double ky = M_PI/10;
-    double p0 = 1.0;        //This I suppose is somewhat arbitrary
+    double p0 = 1.0;        //This choice I suppose is somewhat arbitrary, but works fine
     double d0 = 1.0; 
     auto tg_x = [&kx,&ky,&u0](double x,double y){return -u0*sqrt(kx/ky)*cos(kx*x)*sin(ky*y);};
     auto tg_y = [&kx,&ky,&u0](double x,double y){return u0*sqrt(kx/ky)*sin(kx*x)*cos(ky*y);};
     auto tg_d = [&](double x,double y){return p0 + 3*0.25*d0*pow(u0,2)*(ky*cos(2*kx*x)/kx+kx*cos(2*ky*y)/ky);};
-    //vec iVel = {0.1,0.1};
+    
+    //------------------|SIMULATION VARIABLES|------------------
+    /* wx and wy are weights used in the calculation of the equilibrium distibution function
+       store once and then access by reference in the relevant functions
+    */
     vec wx = {0.0,1,0.0,-1.0,0.0,1.0/sqrt(2),-1.0/sqrt(2),-1.0/sqrt(2),1.0/sqrt(2)};
     vec wy = {0.0,0.0,1.0,0.0,-1.0,1.0/sqrt(2),1.0/sqrt(2),-1.0/sqrt(2),-1.0/sqrt(2)};
-    vec reflect = {0,3,4,1,2,7,8,5,6};
+    vec reflect = {0,3,4,1,2,7,8,5,6};  //When a particle of velocity v[k] is reflected off a wall, the outgoing particle's velocity v[reflec[k]] is relplaced by v[k].
+    /*In simulation the dist. function is updated by mixing the old distribution function with the equilibrium dist. function
+    calculated with the density and velocity obtained after a streaming step. This determines what proportion of the equilibrium
+    function to mix with the old non equilibrium one. It is proportional to dT/T where dT is the simulation timestep and T is the
+    characteristic relaxation time of the fluid (the fluid relaxes to local equilibrium on a timescale T*/
     double mix = 0.9*pow(10.0,-1.0);
     
+    //Command line inputs for the number of iterations and the lattice dimensions
     int nIter = std::atoi(argv[1]);
     int lx = std::atoi(argv[2]);
     int ly = std::atoi(argv[3]);
-    
     int nSites = lx*ly;
     lattice l;
     
+    //Data is output to a txt file which is then read into python for animating the simulation
     std::ofstream outputFile;
     outputFile.open("velocities.txt");
     outputFile<<lx<<","<<ly<<"\n";
+    
+     //------------------|INITIALIZATION|------------------
     l.reserve(nSites);
     /*Put walls at the extreme left and right boundaries of the system if we wish. For the TG vortex
     simulation I have turned walls off.
@@ -183,13 +159,17 @@ int main(int argc, char *argv[]){
         vec iVel = {tg_x(i%lx,i/lx),tg_y(i%lx,i/lx)};
         if(i%lx==0||i%lx==lx-1){
             l.push_back(latt2D(i,tg_d(i%lx,i/lx),iVel,mix,wx,wy,'f',lx,ly));}   //Allows us to set solid walls in the simulation
-        else{                                                       //with no-slip boundary conditions implemented
+        else{
             l.push_back(latt2D(i,tg_d(i%lx,i/lx),iVel,mix,wx,wy,'f',lx,ly));}
         }   
     
-    double tol = pow(10.0,-10)/nSites;
-    double delta = 10;
-    int iter = 0;
+    /* It is not correct to use any old density, velocity pair to initialize the simulation. This is because the Poisson equation which
+    relates the velocity to the density is not generally satisfied. The approach of Mei. et al. performs a series of collide/stream steps
+    in which the particle density is allowed to relax, keeping the initial velocity fixed. This remedies the issue and removes instabilities
+    */
+    double tol = pow(10.0,-10)/nSites;  //Simulation is started when the change in the distribution function is small. This is the numerical tolerance
+    double delta = 10;                  //The mod square difference in the dist. function between iterations when initializing
+    int iter = 0;                       //To prevent the program from never finishing, we stop after a set number of iterations if the initialization fails
     
     while(delta>tol&&iter<400){
         iter+=1;
@@ -212,11 +192,10 @@ int main(int argc, char *argv[]){
         return 0;
     }
     //Main body of program. Performs a number of streaming and collision operations if the site has fluid in it
-    
     for(int n=0;n<nIter;n++){
         for(int i=0;i<nSites;i++){
             if(l[i].type!='w'){
-            l[i].update('d',wx,wy);
+            l[i].update('d',wx,wy); //Update the macroscopic variables each time
             l[i].update('v',wx,wy);
             l[i].collide(wx,wy,[&mix](double arg1,double arg2){return (1-mix)*arg1 + mix*arg2;});}}
         for(int i=0;i<nSites;i++){
@@ -230,16 +209,3 @@ int main(int argc, char *argv[]){
     outputFile.close();
     return 0;
 }
-    /*
-    For parallelization using tbb, we wish to pass:
-    [&](tbb::blocked_range<int> r){
-        for(int i = r.start();i<r.end();i++){
-            l[i].stream(l,reflect);
-        }}
-    Followed by:
-    [&](tbb::blocked_range<int> r){
-        for(int i = r.start();i<r.end();i++){
-            l[i].collide(wx,wy);
-        }}
-    on each iteration to a tbb map function
-    */
